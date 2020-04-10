654214adeb00b6d83e1b286db2a584716ead4bea
David Chen <chen7david@me.com>
2020-04-10 18:13:56 +0800

    update

---
+++ src/plugins/http.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import store from '../store'
+
+const http = axios.create({
+    baseURL: 'http://localhost:4000',
+    timeout: 6000
+})
+
+http.interceptors.request.use((config) => {
+    config.url = encodeURI(config.url)
+    let accessToken = localStorage.getItem('access-token')
+    if(accessToken != null) config.headers.Authorization = `Bearer ${accessToken}`;
+    return config
+}, (error) => {
+    console.log({'req-error': error})
+    return Promise.reject(error)
+})
+
+http.interceptors.response.use((response) => {
+    const {isCargo, payload, details} = response.data
+    if(isCargo) {
+        console.log(response.data)
+        response.data = payload
+        console.log(details)
+    }
+    return response
+}, (error) => {
+    const { isCargo, details } = error.response.data
+    if(isCargo) {
+        if(details.state == 'validation'){
+            store.dispatch('setValidation', details)
+        }else{
+            store.dispatch('setSnackbar', details)
+        }
+        console.log(details)
+    }
+    return Promise.reject(error)
+})
