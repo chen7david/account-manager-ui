8525ae9a341ebf29e7c85b6a0a66d86c3b016978
David <chen7david@me.com>
2020-04-15 11:03:49 +0800

    update

--- src/views/EmailResend.vue
+++ src/views/EmailResend.vue
@@ -7,33 +7,40 @@
         <v-col align="center" cols="12">
           <v-card tile max-width="400px">
             <v-col cols="12">
-                <v-list-item three-line>
-                  <v-list-item-content>
-                      <div class="overline">resend email verification</div>
-                      <v-list-item-subtitle>A verifiation email was sent to {{email}}, click here to resend.</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>  
-                <v-btn large class="my-5" elevation="0"  block :to="{name:'EmailResend', params:{email: user.email}}">resend</v-btn>
-                <v-divider class="mt-8"/>
-                <v-col>
+
+              <v-list-item three-line>
+                <v-list-item-content>
+                    <div class="overline">resend email verification</div>
+                    <v-list-item-subtitle>A verifiation email was sent to {{email}}, click here to resend.</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>  
+
+              <v-btn large class="my-5" elevation="0"  block :to="{name:'EmailResend', params:{email: user.email}}">resend</v-btn>
+
+              <v-divider class="mt-8"/>
+
+              <v-col>
                 <v-list-item three-line>
                   <v-list-item-content>
                       <div class="overline">update account email</div>
                       <v-list-item-subtitle>Update account email by entering a new one here then pressing update.</v-list-item-subtitle>
                   </v-list-item-content>
                 </v-list-item> 
-                <v-col>
-                  <v-text-field
-                    :error-messages="validate('email')"
-                    label="email"
-                    name="email"
-                    outlined
-                    hint="At least 1 character, no spaces"
-                    v-model="user.email"
-                  ></v-text-field>
-                </v-col>
-                <v-btn :disabled="allowUpdate" large elevation="0" block @click="updateEmail({user, email})">update email</v-btn>
               </v-col>
+
+              <v-col>
+                <v-text-field
+                  :error-messages="validate('email')"
+                  label="email"
+                  name="email"
+                  outlined
+                  hint="At least 1 character, no spaces"
+                  v-model="user.email"
+                ></v-text-field>
+              </v-col>
+
+              <v-btn :disabled="allowUpdate" large elevation="0" block @click="updateEmail({user, email})">update email</v-btn>
+              
             </v-col>
           </v-card>
         </v-col>
